daafb24b1b6ab4f797bf9c711ce98ddd866076c5
Juan Rozas <juanbr.rozas2005@gmail.com>
2024-03-31 19:15:06 -0300

    aula-08-completa

--- EXERCICIO-003/codigo.js
+++ EXERCICIO-003/codigo.js
@@ -1,7 +1,13 @@
-var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-var resultado = 0
+    function calcularMedia(numeros) {
+        var soma = 0;
 
-for (i = 0 ; i <num.length ; i++){
-    if(num[i])
-}
+        for (var i = 0; i < numeros.length; i++) {
+            soma += numeros[i];
+        }
+
+        return soma / numeros.length;
+    }
+
+    alert(calcularMedia(numeros));
